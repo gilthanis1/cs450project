cd960587df3095a78d0dbb168f4d9fa37c9c1178
James Richardson <jmrichardson917@gmail.com>
2017-02-09 21:54:04 -0500

    adjust redirect after update

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,4 +5,8 @@ class UsersController < ApplicationController
 	  @user = current_user
   end
 
+  def after_update_path_for(resource)
+    current_user
+	end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,19 @@
 Rails.application.routes.draw do
-
-  devise_for :users
-	# Site Root Route
-  root 'static_pages#home'
-
   devise_scope :user do
-    get '/edit' => 'devise/registrations#edit', as: 'edit_user'
-    get '/signup' => 'registrations#new', as: 'new_user'
+    get '/edit' => 'devise/registrations#edit', as: 'edit_user_registration'
+    get '/signup' => 'registrations#new', as: 'new_user_registration'
   end
 
-  # User Profile Page Re-define
-  get '/profile',	to: 'users#show', :as => :user
+  devise_for :users, :controllers => { :registrations => :registrations }
+
+  # User Profile Page Re-define, and user_path for Blips
+  get '/profile', to: 'users#show', :as => :user
+
+  # Site Root Route
+  root 'static_pages#home'
 
   # Static Pages Routes
-  get  '/help', 	 to: 'static_pages#help'
-  get  '/about', 	 to: 'static_pages#about' 
+  get  '/help',    to: 'static_pages#help'
+  get  '/about',   to: 'static_pages#about' 
   get  '/contact', to: 'static_pages#contact'
 end
