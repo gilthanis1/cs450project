a17c1431e0ea6a04b9e15de1a06e49302665d927
James Richardson <jmrichardson917@gmail.com>
2017-03-29 20:41:39 -0400

    adjust email for registration

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -38,8 +38,8 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { host: 'localhost:3000' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-    user_name:      'jmrichardson917@gmail.com',
-    password:       'Robyn01072012!!',
+    user_name:      'cs450test@gmail.com',
+    password:       'Test1234!',
     domain:         'localhost:3000',
     address:       'smtp.gmail.com',
     port:          '587',
